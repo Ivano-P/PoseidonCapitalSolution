400b403698b46d069e2f871352fbb540b5d2617d
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-08-03 10:30:24 +0200

    added unit test for bidListController methods

--- src/test/java/com/nnk/springboot/PoseidonCapitalSolutionsApplicationTests.java
+++ src/test/java/com/nnk/springboot/PoseidonCapitalSolutionsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nnk.poseidoncapitalsolutions;
+package com.nnk.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/com/nnk/springboot/controllers/BidListControllerTest.java
@@ -0,0 +1,172 @@
+package com.nnk.springboot.controllers;
+
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.services.BidListService;
+import com.nnk.springboot.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+
+import java.security.Principal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class BidListControllerTest {
+
+    @InjectMocks
+    BidListController bidListController;
+
+    @Mock
+    BidListService bidListService;
+
+    @Mock
+    UserService userService;
+
+    @Mock
+    BindingResult bindingResult;
+
+    @Mock
+    Model model;
+
+    @Mock
+    Principal principal;
+
+    BidList mockBid;
+
+    @BeforeEach
+    void setUp() {
+        mockBid = new BidList();
+        mockBid.setAccount("testAccount");
+        mockBid.setType("mockType");
+        mockBid.setBidQuantity(5.00);
+    }
+
+    @Test
+    void testHome() {
+        //Arrange
+        when(principal.getName()).thenReturn("testUser");
+        when(userService.getUserByUsername(anyString())).thenReturn(new User());
+
+        //Act
+        String viewName = bidListController.home(model, principal);
+
+        //Assert
+        assertThat(viewName).isEqualTo("bidList/list");
+        verify(bidListService, times(1)).getAllBids();
+        verify(userService, times(1)).getUserByUsername(anyString());
+    }
+
+
+    @Test
+    void testAddBidForm(){
+        //Arrange
+        when(principal.getName()).thenReturn("testUser");
+        when(userService.getUserByUsername(anyString())).thenReturn(new User());
+
+        //Act
+        String viewName = bidListController.addBidForm(mockBid ,principal, model);
+
+        //Assert
+        assertThat(viewName).isEqualTo("bidList/add");
+        verify(bidListService, times(1)).getAllBids();
+        verify(userService, times(1)).getUserByUsername(anyString());
+    }
+
+    @Test
+    void testValidate_NoErrors() {
+        // Arrange
+        BindingResult result = mock(BindingResult.class);
+        when(result.hasErrors()).thenReturn(false);
+
+        // Act
+        String viewName = bidListController.validate(mockBid, result, model);
+
+        // Assert
+        assertThat(viewName).isEqualTo("redirect:/bidList/list");
+        verify(bidListService, times(1)).updateBidList(mockBid);
+    }
+
+    @Test
+    void testValidate_HasErrors() {
+        // Arrange
+        BindingResult result = mock(BindingResult.class);
+        when(result.hasErrors()).thenReturn(true);
+
+        // Act
+        String viewName = bidListController.validate(mockBid, result, model);
+
+        // Assert
+        assertThat(viewName).isEqualTo("bidList/add");
+        verify(bidListService, never()).updateBidList(mockBid);
+    }
+
+    @Test
+    void testShowUpdateForm() {
+        // Arrange
+        int id = 1;
+        when(bidListService.getBidById(id)).thenReturn(mockBid);
+
+        // Act
+        String viewName = bidListController.showUpdateForm(id, model);
+
+        // Assert
+        assertThat(viewName).isEqualTo("bidList/update");
+        verify(bidListService, times(1)).getBidById(id);
+    }
+
+    @Test
+    void testUpdateBid() {
+        // Arrange
+        int id = 1;
+        when(bidListService.getBidById(id)).thenReturn(mockBid);
+        when(bindingResult.hasErrors()).thenReturn(false);
+
+        // Act
+        String viewName = bidListController.updateBid(id, mockBid, bindingResult, model);
+
+        // Assert
+        assertThat(viewName).isEqualTo("redirect:/bidList/list");
+        verify(bidListService, times(1)).getBidById(id);
+        verify(bidListService, times(1)).updateBidList(any(BidList.class));
+    }
+
+    @Test
+    void testUpdateBidWithError() {
+        // Arrange
+        int id = 1;
+        when(bindingResult.hasErrors()).thenReturn(true);
+
+        // Act
+        String viewName = bidListController.updateBid(id, mockBid, bindingResult, model);
+
+        // Assert
+        assertThat(viewName).isEqualTo("bidList/update");
+        verify(bidListService, times(0)).getBidById(id);
+        verify(bidListService, times(0)).updateBidList(any(BidList.class));
+    }
+
+    @Test
+    void testDeleteBid() {
+        // Arrange
+        int id = 1;
+
+        doNothing().when(bidListService).deleteBidListById(id);
+
+        // Act
+        String viewName = bidListController.deleteBid(id, model);
+
+        // Assert
+        assertThat(viewName).isEqualTo("redirect:/bidList/list");
+        verify(bidListService, times(1)).deleteBidListById(id);
+    }
+
+
+}
