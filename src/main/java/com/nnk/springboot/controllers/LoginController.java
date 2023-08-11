58179c0c09ce8ed1ec095ef2315a5e420173d71f
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-08-11 12:52:47 +0200

    removed commented ou methods in loginController

--- src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -5,14 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.security.Principal;
-
-
-//@RequestMapping("app")
 @Log4j2
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @Controller
@@ -27,28 +22,5 @@ public class LoginController{
         mav.setViewName("login");
         return mav;
     }
-
-    /*
-    @GetMapping("secure/article-details")
-    public ModelAndView getAllUserArticles() {
-        log.info("getAllUserArticles method called");
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("users", userRepository.findAll());
-        mav.setViewName("user/list");
-        return mav;
-    }
-
-    @GetMapping("error")
-    public ModelAndView error() {
-        log.error("error method called");
-        ModelAndView mav = new ModelAndView();
-        String errorMessage= "You are not authorized for the requested data.";
-        mav.addObject("errorMsg", errorMessage);
-        mav.setViewName("403");
-        return mav;
-    }
-
-     */
-
 
 }
